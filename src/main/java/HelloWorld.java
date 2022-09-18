c49f8889eb504b709ecd890f23a1b39a48132360
rakeshreddy572 <rakeshreddy572@gmail.com>
2022-09-18 16:16:57 +0530

    created changes now

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World");
+		get("/hello", (req, res) -> "deploy janasena to tdp ,package");
 	}
 }
